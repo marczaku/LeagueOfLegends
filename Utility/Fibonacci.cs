f06ef11dfb0ecdd0cf67392eea2358c879763b2a
Marc Zaku <mzaku@hotmail.de>
2024-07-04 15:48:46 +0200

    Fixes bug with fibonacci

--- Utility/Fibonacci.cs
+++ Utility/Fibonacci.cs
@@ -4,7 +4,7 @@ public static class Fibonacci
 {
     public static int Recursive(int n)
     {
-        if (n < 1) return n;
+        if (n < 2) return n;
         
         return Recursive(n - 2) + Recursive(n - 1);
     }
