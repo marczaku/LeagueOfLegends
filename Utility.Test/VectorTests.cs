fae8ed68dc0eba064dddbddbed60427dbb40c139
Marc Zaku <mzaku@hotmail.de>
2024-07-18 15:31:14 +0200

    Add Unit Test

--- Utility.Test/VectorTests.cs
+++ Utility.Test/VectorTests.cs
@@ -13,6 +13,20 @@ public class VectorTests
         // then
         Assert.That(up.y, Is.EqualTo(1));
     }
+
+    [Test]
+    public void Vector3Add()
+    {
+        // given
+        Vector3 a = new(0, 3, 0);
+        Vector3 b = new(2, 1, 0);
+        
+        // when
+        var result = a + b;
+        
+        // then
+        Assert.That(result, Is.EqualTo(new Vector3(2, 4, 0)));
+    }
     
     [Test]
     public void Vector3MagnitudeIsCorrect()
